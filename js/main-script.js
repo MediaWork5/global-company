2e808d166ef2ecfbefa906d3658aa2eb6cc8e332
Muhammad Mostafa Ahmed <39030106+MuhammadMustafa37050@users.noreply.github.com>
2020-06-08 17:59:19 +0200

    edit 3

--- js/main-script.js
+++ js/main-script.js
@@ -2,45 +2,6 @@
 
 $(function(){
     'use strict';
-    /********* body ******/
-    var span1 = $("nav > .container > .row > div:first-child > span:nth-of-type(1)")
-    var span2 = $("nav > .container > .row > div:first-child > span:nth-of-type(2)")
-    $(span2).on("click", function(){
-        $("body, #button-top, nav, .list-perfume, .list-product").toggleClass("light-mode no");
-    });
-    $(span1).on("click", function(){
-        $("body, #button-top, nav, .list-perfume, .list-product").toggleClass("light-mode no");
-    });
-
-
-
-    /****** nav ******/
-    $("nav > .container > .row > div:nth-child(3) > div").on("click", function(){
-
-        var menu_div = $("nav > .container > .row > div:nth-child(2)");
-        if($(menu_div).css("display") == "none") {
-            $("nav > .container > .row > div:nth-child(3) > div:hover > span:nth-child(2)").animate({
-                left: 0
-            },300);
-            menu_div.show(300);
-        }
-        else {
-            menu_div.hide(300);
-            $("nav > .container > .row > div:nth-child(3) > div:hover > span:nth-child(2)").animate({
-                left: -4
-            },300);
-        }
-    })
-    $(window).resize(function(){
-        var sw = $(window).innerWidth();
-        if(sw > 750) {
-            $("nav > .container > .row > div:nth-child(2)").show();
-        }
-        else {
-            $("nav > .container > .row > div:nth-child(2)").hide();
-        }
-    });
-
     /** slick slider**/
     $('.autoplay').slick({
         lazyLoad: 'ondemand',
@@ -53,9 +14,15 @@ $(function(){
 
         responsive: [
             {
+                breakpoint: 1199,
+                settings: {
+                    slidesToShow:4 
+                }
+            },
+            {
                 breakpoint: 991,
                 settings: {
-                    slidesToShow: 5
+                    slidesToShow:3 
                 }
             },
             {
